01266d41880439a01560eea15e91fd05cc2a1565
EC2 Default User <ec2-user@ip-10-0-0-43.ap-northeast-1.compute.internal>
2022-08-22 07:12:47 +0000

    changes for v2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+        <h1>Hello from V2</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
